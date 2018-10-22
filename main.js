2a9d1e1033fabe80d43d768e511cd18f35fd34d9
Micah Caffey <mrcaffey@gmail.com>
2018-10-22 11:08:44 -0600

    updated functions

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 //Action Creators
 const ADD_ENTRY = 'ADD_ENTRY'
+const REMOVE_ENTRY = 'REMOVE_ENTRY'
+const ADD_ITEM = 'ADD_ITEM'
 
 //Actions
 const addEntry = (entry) => {
   return { type: ADD_ENTRY, entry }
 }
 
-const addCredit = (credit) => {
-  return { type: ADD_CREDIT, credit: {...credit, type: 'Credit'} }
+const removeEntry = (index) => {
+  return { type: REMOVE_ENTRY, index }
+}
+
+const addItem = (item) => {
+  return { type: ADD_ITEM, item }
 }
 
 //Reducer
+//1. wish list items
+//2. ledger
+
+
+const items = ( state = [], action ) => {
+  switch(action.type) {
+    case ADD_ITEM:
+      return [...state, action.item]
+    default: 
+      return state
+  }
+}
+
 const ledger = (state = [], action) => {
   switch(action.type) {
     case ADD_ENTRY:
       return [...state, action.entry]
+    case REMOVE_ENTRY:
+      return state.filter( (_, i) => i !== action.index )
     default:
       return state
   }
@@ -38,33 +59,77 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION && window.__REDUX_DEVTOOLS_EXTENSION()
 )
 
+const sumEntries = () => {
+  const h1 = document.getElementById('total')
+  h1.innerHTML = null
+  const { ledger } = store.getState()
+  const value = ledger.reduce( (total, entry) => {
+    const amt = parseFloat(entry.amt)
+    if (entry.type === 'Credit')
+      return total + amt
+    return total - amt
+  }, 0)
+
+  h1.innerHTML = `$${value}`
+}
+
 const updateHistory = () => {
   const list = document.getElementById('history')
   const entries = store.getState().ledger
   list.innerHTML = null
-  entries.forEach( (entry) => {
+  entries.forEach( (entry, index) => {
     const item = document.createElement('li')
-    item.innerHTML = `$${entry.amt} - ${entry.description}`
+    const span = document.createElement('span')
+    const button = document.createElement('button')
+    span.innerHTML = `$${entry.amt} - ${entry.description}`
+    button.addEventListener('click', () => store.dispatch(removeEntry(index)) )
+    button.innerText = 'Remove'
+    span.appendChild(button)
+    item.appendChild(span)
     item.className = entry.type
     list.append(item)
   })
 }
 
-const handleSubmit= (e) => {
+
+const formElements = (form) => {
+  const obj = {}
+  for (let el of form.elements) {
+    if (el.name)
+      obj[el.name]  = el.value
+    }
+
+      return obj
+  }
+
+
+
+
+const handleSubmit = (e) => {
   e.preventDefault()
   const obj = {}
   const form = e.target
-  for (let el of form.elements) {
-    if (el.name)
-    obj[el.name] = encodeURIComponent(el.value)
-  }
+  const obj = formElements(form)
 
   store.dispatch(addEntry(obj))
-  updateHistory()
   form.reset()
 }
 
-store.subscribe(updateHistory)
+const handleItemForm = (e) => {
+  e.preventDefault()
+  const form = e.target
+  const obj = formElements(form)
+  store.dispatch(addItem(obj))
+  form.reset()
+}
 
+const log = () => {
+  console.log( store.getState().ledger )
+}
+
+
+store.subscribe(updateHistory)
+store.subscribe(sumEntries)
+store.subscribe(log)
 
 document.getElementById('add_entry').addEventListener('submit', handleSubmit)
