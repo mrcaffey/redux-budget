3395fc48b2c89b990ce2368faafcf95132847f1a
Micah Caffey <mrcaffey@gmail.com>
2018-10-23 10:07:50 -0600

    updated LedgerForm.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import Ledger from './Ledger'
 import WishList from './WishList'
+
+const Container = styled.div`
+  display:flex;
+  justify-content: space-around;
+`
 
 
 const App = () => (
   <div>
-    <Ledger />
-    <WishList />
+    <Container>
+      <Ledger />
+      <WishList />
+    </Container>
   </div>
 )
 
